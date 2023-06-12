8b804fb227ba5ef3694cac9fddcce0e0a94c3bdf
YanaVesna <kydves78@web.de>
2023-06-12 13:08:07 +0200

    gitcommit1

--- src/components/CartFurniture.jsx
+++ src/components/CartFurniture.jsx
@@ -4,18 +4,24 @@ function CartFurniture({
   id,
   category,
   title,
-  image,
+  img,
   price,
   description,
   rating,
 }) {
   return (
     <div className="furniture__box__card">
-      <img src={`/${category}/${category}_${id + 1}.png`} alt="furniture" />
+      {/* <img src={`/${category}/${category}_${id + 1}.png`}  */}
+      <img src={img} alt="furniture" />
       <div className="furniture__box__card__direction">
         <div className="furniture__box__card__direction__name">
           <h2>{title}</h2>
-          <img width={120} height={22} src="/stars.png" alt="" />
+          <img
+            width={120}
+            height={22}
+            src="https://i.ibb.co/Yjj388F/stars.png"
+            alt=""
+          />
         </div>
         <h3>Price : {price} $</h3>
         <span>Description</span>
